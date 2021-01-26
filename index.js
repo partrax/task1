91dd1fcb8a9198e72b91c62627f5783ac2811fdc
partrax <77982406+partrax@users.noreply.github.com>
2021-01-26 13:05:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ $ git checkout -b [feature-test]
 git add index.html
 git add config.js
 git commit -m
+git checkout master
+git pull origin master
+git merge feature-initiate
+git push origin master
